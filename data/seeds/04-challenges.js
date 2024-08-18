6a4eb3868482042f546b8dd9e239f7e3649ba6b8
Robert Toth-Ludanyi <roberttothmails@yahoo.com>
2024-08-18 11:28:23 +0200

    added data

--- data/seeds/04-challenges.js
+++ data/seeds/04-challenges.js
@@ -1,7 +1,427 @@
-// const challenges = [
-    
-// ]
+const challenges = [
+    {
+        topic_id: 1,
+        order_index: 1,
+        challenge_name: 'add',
+        difficulty: 1,
+        description: 'Write a function that returns the sum of two numbers. Example: For param1 = 1 and param2 = 2, the output should be solution(param1, param2) = 3. Input/Output [execution time limit] 4 seconds (js) [memory limit] 1 GB [input] integer param1 Guaranteed constraints: -1000 ≤ param1 ≤ 1000. [input] integer param2 Guaranteed constraints: -1000 ≤ param2 ≤ 1000. [output] integer The sum of the two inputs.'
+    },
+    {
+        topic_id: 1,
+        order_index: 2,
+        challenge_name: 'centuryFromYear',
+        difficulty: 1,
+        description: 'Given a year, return the century it is in. The first century spans from the year 1 up to and including the year 100, the second - from the year 101 up to and including the year 200, etc. Example: For year = 1905, the output should be solution(year) = 20; For year = 1700, the output should be solution(year) = 17. Input/Output [execution time limit] 4 seconds (js) [memory limit] 1 GB [input] integer year A positive integer, designating the year. Guaranteed constraints: 1 ≤ year ≤ 2005. [output] integer The number of the century the year is in.'
+    },
+    {
+        topic_id: 1,
+        order_index: ,
+        challenge_name: ,
+        difficulty: ,
+        description: 
+    },
+    {
+        topic_id: 1,
+        order_index: ,
+        challenge_name: ,
+        difficulty: ,
+        description: 
+    },
+    {
+        topic_id: 1,
+        order_index: ,
+        challenge_name: ,
+        difficulty: ,
+        description: 
+    },
+    {
+        topic_id: 1,
+        order_index: ,
+        challenge_name: ,
+        difficulty: ,
+        description: 
+    },
+    {
+        topic_id: 1,
+        order_index: ,
+        challenge_name: ,
+        difficulty: ,
+        description: 
+    },
+    {
+        topic_id: 1,
+        order_index: ,
+        challenge_name: ,
+        difficulty: ,
+        description: 
+    },
+    {
+        topic_id: 1,
+        order_index: ,
+        challenge_name: ,
+        difficulty: ,
+        description: 
+    },
+    {
+        topic_id: 1,
+        order_index: ,
+        challenge_name: ,
+        difficulty: ,
+        description: 
+    },
+    {
+        topic_id: 1,
+        order_index: ,
+        challenge_name: ,
+        difficulty: ,
+        description: 
+    },
+    {
+        topic_id: 1,
+        order_index: ,
+        challenge_name: ,
+        difficulty: ,
+        description: 
+    },
+    {
+        topic_id: 1,
+        order_index: ,
+        challenge_name: ,
+        difficulty: ,
+        description: 
+    },
+    {
+        topic_id: 1,
+        order_index: ,
+        challenge_name: ,
+        difficulty: ,
+        description: 
+    },
+    {
+        topic_id: 1,
+        order_index: ,
+        challenge_name: ,
+        difficulty: ,
+        description: 
+    },
+    {
+        topic_id: 1,
+        order_index: ,
+        challenge_name: ,
+        difficulty: ,
+        description: 
+    },
+    {
+        topic_id: 1,
+        order_index: ,
+        challenge_name: ,
+        difficulty: ,
+        description: 
+    },
+    {
+        topic_id: 1,
+        order_index: ,
+        challenge_name: ,
+        difficulty: ,
+        description: 
+    },
+    {
+        topic_id: 1,
+        order_index: ,
+        challenge_name: ,
+        difficulty: ,
+        description: 
+    },
+    {
+        topic_id: 1,
+        order_index: ,
+        challenge_name: ,
+        difficulty: ,
+        description: 
+    },
+    {
+        topic_id: 1,
+        order_index: ,
+        challenge_name: ,
+        difficulty: ,
+        description: 
+    },
+    {
+        topic_id: 1,
+        order_index: ,
+        challenge_name: ,
+        difficulty: ,
+        description: 
+    },
+    {
+        topic_id: 1,
+        order_index: ,
+        challenge_name: ,
+        difficulty: ,
+        description: 
+    },
+    {
+        topic_id: 1,
+        order_index: ,
+        challenge_name: ,
+        difficulty: ,
+        description: 
+    },
+    {
+        topic_id: 1,
+        order_index: ,
+        challenge_name: ,
+        difficulty: ,
+        description: 
+    },
+    {
+        topic_id: 1,
+        order_index: ,
+        challenge_name: ,
+        difficulty: ,
+        description: 
+    },
+    {
+        topic_id: 1,
+        order_index: ,
+        challenge_name: ,
+        difficulty: ,
+        description: 
+    },
+    {
+        topic_id: 1,
+        order_index: ,
+        challenge_name: ,
+        difficulty: ,
+        description: 
+    },
+    {
+        topic_id: 1,
+        order_index: ,
+        challenge_name: ,
+        difficulty: ,
+        description: 
+    },
+    {
+        topic_id: 1,
+        order_index: ,
+        challenge_name: ,
+        difficulty: ,
+        description: 
+    },
+    {
+        topic_id: 1,
+        order_index: ,
+        challenge_name: ,
+        difficulty: ,
+        description: 
+    },
+    {
+        topic_id: 1,
+        order_index: ,
+        challenge_name: ,
+        difficulty: ,
+        description: 
+    },
+    {
+        topic_id: 1,
+        order_index: ,
+        challenge_name: ,
+        difficulty: ,
+        description: 
+    },
+    {
+        topic_id: 1,
+        order_index: ,
+        challenge_name: ,
+        difficulty: ,
+        description: 
+    },
+    {
+        topic_id: 1,
+        order_index: ,
+        challenge_name: ,
+        difficulty: ,
+        description: 
+    },
+    {
+        topic_id: 1,
+        order_index: ,
+        challenge_name: ,
+        difficulty: ,
+        description: 
+    },
+    {
+        topic_id: 1,
+        order_index: ,
+        challenge_name: ,
+        difficulty: ,
+        description: 
+    },
+    {
+        topic_id: 1,
+        order_index: ,
+        challenge_name: ,
+        difficulty: ,
+        description: 
+    },
+    {
+        topic_id: 1,
+        order_index: ,
+        challenge_name: ,
+        difficulty: ,
+        description: 
+    },
+    {
+        topic_id: 1,
+        order_index: ,
+        challenge_name: ,
+        difficulty: ,
+        description: 
+    },
+    {
+        topic_id: 1,
+        order_index: ,
+        challenge_name: ,
+        difficulty: ,
+        description: 
+    },
+    {
+        topic_id: 1,
+        order_index: ,
+        challenge_name: ,
+        difficulty: ,
+        description: 
+    },
+    {
+        topic_id: 1,
+        order_index: ,
+        challenge_name: ,
+        difficulty: ,
+        description: 
+    },
+    {
+        topic_id: 1,
+        order_index: ,
+        challenge_name: ,
+        difficulty: ,
+        description: 
+    },
+    {
+        topic_id: 1,
+        order_index: ,
+        challenge_name: ,
+        difficulty: ,
+        description: 
+    },
+    {
+        topic_id: 1,
+        order_index: ,
+        challenge_name: ,
+        difficulty: ,
+        description: 
+    },
+    {
+        topic_id: 1,
+        order_index: ,
+        challenge_name: ,
+        difficulty: ,
+        description: 
+    },
+    {
+        topic_id: 1,
+        order_index: ,
+        challenge_name: ,
+        difficulty: ,
+        description: 
+    },
+    {
+        topic_id: 1,
+        order_index: ,
+        challenge_name: ,
+        difficulty: ,
+        description: 
+    },
+    {
+        topic_id: 1,
+        order_index: ,
+        challenge_name: ,
+        difficulty: ,
+        description: 
+    },
+    {
+        topic_id: 1,
+        order_index: ,
+        challenge_name: ,
+        difficulty: ,
+        description: 
+    },
+    {
+        topic_id: 1,
+        order_index: ,
+        challenge_name: ,
+        difficulty: ,
+        description: 
+    },
+    {
+        topic_id: 1,
+        order_index: ,
+        challenge_name: ,
+        difficulty: ,
+        description: 
+    },
+    {
+        topic_id: 1,
+        order_index: ,
+        challenge_name: ,
+        difficulty: ,
+        description: 
+    },
+    {
+        topic_id: 1,
+        order_index: ,
+        challenge_name: ,
+        difficulty: ,
+        description: 
+    },
+    {
+        topic_id: 1,
+        order_index: ,
+        challenge_name: ,
+        difficulty: ,
+        description: 
+    },
+    {
+        topic_id: 1,
+        order_index: ,
+        challenge_name: ,
+        difficulty: ,
+        description: 
+    },
+    {
+        topic_id: 1,
+        order_index: ,
+        challenge_name: ,
+        difficulty: ,
+        description: 
+    },
+    {
+        topic_id: 1,
+        order_index: ,
+        challenge_name: ,
+        difficulty: ,
+        description: 
+    },
+    {
+        topic_id: 1,
+        order_index: ,
+        challenge_name: ,
+        difficulty: ,
+        description: 
+    },
+
+]
 
 exports.seed = async function (knex) {
-    // await knex('challenges').insert(challenges)
+    await knex('challenges').insert(challenges)
 }
