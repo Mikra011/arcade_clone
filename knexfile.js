a30e1682144d9c189951c393cdd01300147150f5
Robert Toth-Ludanyi <roberttothmails@yahoo.com>
2024-09-09 16:56:14 +0200

    🐛 FIX: small fixes for production

--- knexfile.js
+++ knexfile.js
@@ -1,32 +1,41 @@
-const sharedConfig = {
-    client: 'sqlite3',
-    useNullAsDefault: true,
+const sqliteConfig = {
+  client: 'sqlite3',
+  useNullAsDefault: true,
+  migrations: {
+    directory: './data/migrations',
+  },
+  seeds: {
+    directory: './data/seeds',
+  },
+  pool: {
+    afterCreate: (conn, done) => {
+      conn.run('PRAGMA foreign_keys = ON', done);
+    },
+  },
+}
+
+module.exports = {
+  development: {
+    ...sqliteConfig,
+    connection: { filename: './data/arcade_clone.db3' },
+  },
+  testing: {
+    ...sqliteConfig,
+    connection: { filename: './data/arcade_clone.test.db3' },
+  },
+  production: {
+    client: 'pg',
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
     migrations: {
       directory: './data/migrations',
     },
     seeds: {
       directory: './data/seeds',
     },
-    // this enables foreign keys in SQLite
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run('PRAGMA foreign_keys = ON', done)
-      },
-    },
-  }
-  
-  module.exports = {
-    development: {
-      ...sharedConfig,
-      connection: { filename: './data/arcade_clone.db3' },
-    },
-    testing: {
-      ...sharedConfig,
-      connection: { filename: './data/arcade_clone.test.db3' },
-    },
-    production: {
-      ...sharedConfig,
-      client: 'pg',
-      connection: process.env.DATABASE_URL, // PostgreSQL URL provided by Render
-    },
-  }
+  },
+}
